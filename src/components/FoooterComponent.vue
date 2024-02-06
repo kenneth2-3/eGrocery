f7255e1234c7ba2843c721e2036bfd465cc0c48d
kenneth2-3 <deborah2020.kenneth@gmail.com>
2024-02-06 13:57:01 +0100

    grocery

---
+++ src/components/FoooterComponent.vue
@@ -0,0 +1,69 @@
+<template>
+    <v-footer dark padless>
+        <v-card flat tile class="black white--text py-12 px-5" width="100%">
+            <v-row>
+                <v-col cols="12" xs="12" sm="6" md="4" class="pa-16">
+                    <v-toolbar color="transparent" flat>
+                        <v-badge color="#D5F0DB" dot>
+                            <v-img src="1.png" contain width="30"></v-img>
+                        </v-badge>
+                        <v-toolbar-title class="ml-4">
+                            <span class="green--text">e</span><strong>Grocery</strong>
+                        </v-toolbar-title>
+                    </v-toolbar>
+                    <v-card-text class="pt-0">
+                        <v-btn v-for="icon in icons" :key="icon" class="mx-1 white--text" icon fab outlined small>
+                            <v-icon size="20px">
+                                {{ icon }}
+                            </v-icon>
+                        </v-btn>
+                    </v-card-text>
+                </v-col>
+                <v-col cols="12" xs="12" sm="6" md="2">
+                    <v-card-text class="white--text pt=0">
+                        <h3>About</h3>
+                    </v-card-text>
+                    <v-card-text class="grey--text"> How it works </v-card-text>
+                    <v-card-text class="grey--text mt-n4"> Our Programs </v-card-text>
+                    <v-card-text class="grey--text mt-n4"> Promotions </v-card-text>
+                    <v-card-text class="grey--text mt-n4"> Refer a Friend </v-card-text>
+                </v-col>
+                <v-col cols="12" xs="12" sm="6" md="2">
+                    <v-card-text class="white--text pt=0">
+                        <h3>Delivery</h3>
+                    </v-card-text>
+                    <v-card-text class="grey--text"> Cost on Delivery </v-card-text>
+                    <v-card-text class="grey--text mt-n4"> Payment Method </v-card-text>
+                    <v-card-text class="grey--text mt-n4"> Delivery Status </v-card-text>
+                    <v-card-text class="grey--text mt-n4"> Returns </v-card-text>
+                </v-col>
+                <v-col cols="12" xs="12" sm="6" md="4">
+                    <v-card-text class="white--text pt=0">
+                        <h3>Contact</h3>
+                    </v-card-text>
+                    <v-card-text class="grey--text"> AAE IdeaPro </v-card-text>
+                    <v-card-text class="grey--text mt-n4"> 216 / C EAST Road </v-card-text>
+                    <v-card-text class="grey--text mt-n4"> ada.ideadpro.com </v-card-text>
+                    <v-card-text class="grey--text mt-n4"> (+234) 9036459815 </v-card-text>
+                </v-col>
+            </v-row>
+        </v-card>
+    </v-footer>
+</template>
+
+<script>
+export default {
+    data: () => ({
+        icons: [
+            "fab fa-apple",
+            "fab fa-google-plus-g",
+            "fab fa-twitter",
+            "fab fa-facebook-f"
+        ],
+    }),
+}
+</script>
+
+<style>
+
+</style>
